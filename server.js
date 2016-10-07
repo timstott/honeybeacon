e0d1f2e6c70b6034d40a4d81846c708e82d77c41
Timothy Stott <stott.timothy@gmail.com>
2016-10-07 16:37:33 +0100

    Create /devices endpoint
    This endpoint is called by the beacon devices.
    
    Add test scenario when gist configuration cannot be found on GitHub

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 'use strict';
 
 import express from 'express';
+import https from 'https';
 const app = express();
 const serverPort = process.env.PORT || 3000;
+
+const noFaultsFoundCode = 0;
 
 app.use((_, res, next) => {
   res.set('Content-Type', 'text/plain')
@@ -15,7 +18,43 @@ app.get('/', (req, res) => {
 
 app.get('/ping', (_, res) => { res.send("pong\n"); });
 
-if(!module.parent){ 
+
+const fetchGist = (gistId) => {
+  return new Promise((_, reject) => {
+    const gistId  = process.env.CONFIG_GIST_ID;
+    const gistUrl = `https://api.github.com/gists/${gistId}`;
+
+    makeGetRequest(gistUrl).catch(err => {
+      reject(new Error('Failed to fetch gist'));
+    });
+  });
+};
+
+const makeGetRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    const request = https.get(url, (response) => {
+      const {statusCode} = response;
+
+      if (statusCode != 200) {
+        const errorMessage = `Failed GET request ${url}. Response status code ${statusCode}`;
+        console.log(errorMessage);
+        reject(new Error(errorMessage));
+      }
+      const body = [];
+      response.on('data', (chunk) => body.push(chunk));
+      response.on('end', () => resolve(body.join('')));
+    });
+    request.on('error', (err) => reject(err));
+  });
+};
+
+app.get('/devices', (req, res) => {
+  fetchGist().catch(err => {
+    res.send(`${err.message}\n${noFaultsFoundCode}\n`);
+  });
+});
+
+if (!module.parent) {
   app.listen(serverPort, () => {
     console.log(`Honeybeacon listening on port ${serverPort}`);
   });
--- test/server.js
+++ test/server.js
@@ -1,9 +1,12 @@
+'use strict';
+
 process.env.NODE_ENV = 'test';
 process.env.PORT     = 3333;
 
 import chai, {expect} from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../server';
+import nock from 'nock';
 
 chai.use(chaiHttp);
 
@@ -17,5 +20,31 @@ describe('/ping', () => {
         expect(res.text).to.equal("pong\n");
         done();
       });
+  });
+});
+
+describe('/devices', () => {
+  const api = nock('https://api.github.com');
+  const request = chai.request(app).get('/devices');
+
+  afterEach(() => nock.cleanAll());
+
+  context('when the gist cannot be found on GitHub', () => {
+    beforeEach(() => {
+      process.env.CONFIG_GIST_ID = 'unknown-gist-id';
+
+      api.get('/gists/unknown-gist-id').reply(404, {
+        "message": "Not Found",
+        "documentation_url": "https://developer.github.com/v3"
+      });
+    });
+
+    it('responds as no faults', (done) => {
+        request.end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.text).to.equal("Failed to fetch gist\n0\n");
+          done();
+        });
+    });
   });
 });
